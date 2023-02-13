c06288bac228f7efd2bb721b271613c8859fe365
Olena Yastrebtsova <78073513+yastrb@users.noreply.github.com>
2023-02-13 11:38:37 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
 
